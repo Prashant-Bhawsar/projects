ff85fc6993cb1f9db394f500bc781ccaacdc081f
Your Name <you@example.com>
2024-01-29 17:06:43 +0530

    signin api route

--- server/api/controllers/auth.controller.js
+++ server/api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-import { nextTick } from "process";
+import { errorHandler } from "../utils/error.js";
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
   const encryptedPassword = bcryptjs.hashSync(password, 10);
@@ -12,5 +12,24 @@ export const signUp = async (req, res, next) => {
     });
   } catch (err) {
     next(err);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong crendentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); // 1hour
+    res
+      .cookie("access_token", token, { httponly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
